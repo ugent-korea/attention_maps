b9c4f64b8a2c299b537dadd593f47c2f27eeb2b3
Wondaeng <rktvna@gmail.com>
2024-06-15 02:09:14 +0900

    Unified functions for IOU and pointing game

--- src/main.py
+++ src/main.py
@@ -45,80 +45,64 @@ def generate_interpretability(dataset_type, init_type, method, device):
         print(f'Saving f{save_path}')
 
 
-# I really don't like current structure, will be revised. 
+# I don't like current structure: will be revised.
+def generate_evaluation(dataset_type, evaluation_type, top_perc=None):
+    annot_mask_src = f"../data/224_mask_annot_from_dr/{dataset_type}"
+
+    methods = ["gradcam", "attention", "chefer"]
+    init_types = ["scratch", "sup", "dino", "mae"]
+
+    annot_mask_list = os.listdir(annot_mask_src)
+    eval_dict_method = dict()
+    for method in methods:
+        method_map_src = f"./output/{method}"
+        eval_dict_init = dict()
+        for init_type in init_types:
+            eval_lst = list()
+            for i in annot_mask_list:
+                img_name = i.rstrip(".png")
+                annot_path = os.path.join(annot_mask_src, i)
+
+                annot_map = mask_to_npy(annot_path)
+                inter_map = np.load(os.path.join(method_map_src, f"{dataset_type}_{init_type}_{img_name}.npy"))
+
+                if evaluation_type == 'iou':
+                    bin_map = binarize_inter_map(inter_map, top_perc)
+                    rect_map = binary_map_to_rect_mask(bin_map)
+                    eval_value = intersection_over_union(annot_map, rect_map)
+                elif evaluation_type == 'pg':
+                    eval_value = pixel_wise_pointing_game(inter_map, annot_map)
+                
+                eval_lst.append(eval_value)
+            
+            if evaluation_type == 'iou':
+                eval_mean = np.mean(eval_lst)
+            elif evaluation_type == 'pg':
+                eval_mean = np.sum(eval_lst) / len(annot_mask_list)
+                
+            eval_dict_init[init_type] = eval_mean
+        eval_dict_method[method] = eval_dict_init
+
+    # Convert the dictionary to a DataFrame
+    eval_df = pd.DataFrame(eval_dict_method).T  # Transpose for better format
+
+    # Save the DataFrame to a CSV file
+    os.makedirs("./output/evaluation/", exist_ok=True)
+    if evaluation_type == 'iou':
+        csv_filename = f'./output/evaluation/iou{top_perc}_mean_{dataset_type}.csv'
+    elif evaluation_type == 'pg':
+        csv_filename = f'./output/evaluation/pg_{dataset_type}.csv'
+    
+    eval_df.to_csv(csv_filename, index=True)      
+    print(f"Results saved to {csv_filename}")
+
+
 def generate_iou_evaluation(dataset_type, top_perc):
-    annot_mask_src = f"../data/224_mask_annot_from_dr/{dataset_type}"
-
-    methods = ["gradcam", "attention", "chefer"]
-    init_types = ["scratch", "sup", "dino", "mae"]
-
-    annot_mask_list = os.listdir(annot_mask_src)
-    iou_dict_method = dict()
-    for method in methods:
-        method_map_src = f"./output/{method}"
-        iou_dict_init = dict()
-        for init_type in init_types:
-            iou_lst = list()
-            for i in annot_mask_list:
-                img_name = i.rstrip(".png")
-                annot_path = os.path.join(annot_mask_src, i)
-
-                annot_map = mask_to_npy(annot_path)
-                inter_map = np.load(os.path.join(method_map_src, f"{dataset_type}_{init_type}_{img_name}.npy"))
-
-                bin_map = binarize_inter_map(inter_map, top_perc)
-                rect_map = binary_map_to_rect_mask(bin_map)
-                iou = intersection_over_union(annot_map, rect_map)
-                iou_lst.append(iou)
-            iou_mean = np.mean(iou_lst)
-            iou_dict_init[init_type] = iou_mean
-        iou_dict_method[method] = iou_dict_init
-
-    # Convert the dictionary to a DataFrame
-    iou_df = pd.DataFrame(iou_dict_method).T  # Transpose for better format
-
-    # Save the DataFrame to a CSV file
-    os.makedirs("./output/evaluation/", exist_ok=True)
-    csv_filename = f'./output/evaluation/iou{top_perc}_mean_{dataset_type}.csv'
-    iou_df.to_csv(csv_filename, index=True)      
-    print(f"Results saved to {csv_filename}")
+    generate_evaluation(dataset_type, 'iou', top_perc)
 
 
-# I really don't like current structure, will be revised. 
 def generate_pg_evaluation(dataset_type):
-    annot_mask_src = f"../data/224_mask_annot_from_dr/{dataset_type}"
-
-    methods = ["gradcam", "attention", "chefer"]
-    init_types = ["scratch", "sup", "dino", "mae"]
-
-    annot_mask_list = os.listdir(annot_mask_src)
-    pg_dict_method = dict()
-    for method in methods:
-        method_map_src = f"./output/{method}"
-        pg_dict_init = dict()
-        for init_type in init_types:
-            pg_lst = list()
-            for i in annot_mask_list:
-                img_name = i.rstrip(".png")
-                annot_path = os.path.join(annot_mask_src, i)
-
-                annot_map = mask_to_npy(annot_path)
-                inter_map = np.load(os.path.join(method_map_src, f"{dataset_type}_{init_type}_{img_name}.npy"))
-
-                hit_or_miss = pixel_wise_pointing_game(inter_map, annot_map)
-                pg_lst.append(hit_or_miss)
-            pg_mean = np.sum(pg_lst) / len(annot_mask_list)
-            pg_dict_init[init_type] = pg_mean
-        pg_dict_method[method] = pg_dict_init
-
-    # Convert the dictionary to a DataFrame
-    pg_df = pd.DataFrame(pg_dict_method).T  # Transpose for better format
-
-    # Save the DataFrame to a CSV file
-    os.makedirs("./output/evaluation/", exist_ok=True)
-    csv_filename = f'./output/evaluation/pg_{dataset_type}.csv'
-    pg_df.to_csv(csv_filename, index=True)      
-    print(f"Results saved to {csv_filename}")
+    generate_evaluation(dataset_type, 'pg')
 
 
 
